b49a5508e4a1ab5a026cecf989a720d4a32fe768
Najlae Abarghache <najlae.abarghache1@gmail.com>
2024-08-26 19:19:30 +0100

    deploying to vercel

--- src/api/AuthRequest.js
+++ src/api/AuthRequest.js
@@ -2,17 +2,5 @@ import axios from 'axios'
 
 const API = axios.create({ baseURL: 'https://media-backend-ymug.vercel.app' })
 
-export const logIn = (formData) =>
-  API.post('/auth/login', formData, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-    },
-  })
-export const signUp = (formData) =>
-  API.post('/auth/register', formData, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-    },
-  })
+export const logIn = (formData) => API.post('/auth/login', formData)
+export const signUp = (formData) => API.post('/auth/register', formData)
