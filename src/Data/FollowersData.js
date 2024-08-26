6b7fd96a9a1cde607329f3e15de9df24fbc2b45c
Najlae Abarghache <najlae.abarghache1@gmail.com>
2024-08-26 19:05:40 +0100

    deploying to vercel

--- src/Data/FollowersData.js
+++ src/Data/FollowersData.js
@@ -4,8 +4,8 @@ import img3 from '../img/img3.png'
 import img4 from '../img/img4.jpg'
 
 export const Followers = [
-    {name: "Andrew Thomas", username: "andrewthomas", img: img1},
-    {name: "Hulk Buster", username: "hulky19", img: img2},
-    {name: "Thor", username: "thundermaster", img: img3},
-    {name: "Natasha", username: "natasha98", img: img4}
-];
+  { name: 'Andrew Thomas', username: 'andrewthomas', img: img1 },
+  { name: 'Hulk Buster', username: 'hulky19', img: img2 },
+  { name: 'Thor', username: 'thundermaster', img: img3 },
+  { name: 'Natasha', username: 'natasha98', img: img4 },
+]
